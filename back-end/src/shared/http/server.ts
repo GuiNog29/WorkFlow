f25151fdf872faac7e95b1fa59967388b071e980
Guilherme Almeida <guimathiasgui@gmail.com>
2023-10-22 00:15:03 -0300

    Criando conexão com banco de dados e executando sistema

--- back-end/src/shared/http/server.ts
+++ back-end/src/shared/http/server.ts
@@ -1,12 +1,12 @@
 import 'reflect-metadata';
-import 'dotenv/config'
-import  app  from './app';
-// import { dataSource } from '../Shared/Typeorm';
+import 'dotenv/config';
+import app from './app';
+import { dataSource } from '@shared/typeorm';
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-// dataSource.initialize().then(() => {
-//   const server = app.listen(port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
-//   });
-// });
+dataSource.initialize().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+});
