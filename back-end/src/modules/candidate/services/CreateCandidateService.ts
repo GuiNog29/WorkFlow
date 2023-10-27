538a79f9a520b86abdde5916f6dd8a5ecce3a07c
Guilherme Almeida <guimathiasgui@gmail.com>
2023-10-26 23:22:17 -0300

    Criando validacao de cpf e email para candidato

--- back-end/src/modules/candidate/services/CreateCandidateService.ts
+++ back-end/src/modules/candidate/services/CreateCandidateService.ts
@@ -1,7 +1,7 @@
-import { UserService } from 'src/services/UserService';
 import { ICandidate } from '../domain/models/ICandidate';
 import { CandidateRepository } from '../infra/typeorm/repositories/CandidateRepository';
 import { Candidate } from '../infra/typeorm/entities/Candidate';
+import { ValidCandidateExistService } from './ValidCandidateExistService';
 
 export class CreateCandidateService {
   private candidateRepository: CandidateRepository;
@@ -11,8 +11,8 @@ export class CreateCandidateService {
   }
 
   public async excecute({ name, cpf, email, password }: ICandidate): Promise<Candidate> {
-    const userService = new UserService();
-    await userService.validUserExist('candidate', { cpf, email });
+    const validCandidateExistService = new ValidCandidateExistService();
+    await validCandidateExistService.execute(cpf, email);
 
     return await this.candidateRepository.create({ name, cpf, email, password });
   }
