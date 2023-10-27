7fd71afe34069caac76c06f8ac7fe5f11e67826f
Guilherme Almeida <guimathiasgui@gmail.com>
2023-10-26 23:24:25 -0300

    Criando servico para buscar empregador

---
+++ back-end/src/modules/employer/services/GetEmployerByIdService.ts
@@ -0,0 +1,19 @@
+import { AppError } from '@shared/errors/AppError';
+import { Employer } from '../infra/typeorm/entities/Employer';
+import { EmployerRepository } from '../infra/typeorm/repositories/EmployerRepository';
+
+export class GetEmployerByIdService {
+  private employerRepository: EmployerRepository;
+
+  constructor() {
+    this.employerRepository = new EmployerRepository();
+  }
+
+  async execute(employerId: number): Promise<Employer | null> {
+    const employer = this.employerRepository.getEmployerById(employerId);
+
+    if (!employer) throw new AppError('Usuário não encontrado.');
+
+    return employer;
+  }
+}
