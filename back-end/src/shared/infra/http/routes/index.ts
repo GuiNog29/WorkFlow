0803c0636c183e74f1fab62dfb85a8e5a116d973
Guilherme Almeida <guimathiasgui@gmail.com>
2023-10-30 22:34:43 -0300

    Criando rotas e corrigindo pontas soltas

--- back-end/src/shared/infra/http/routes/index.ts
+++ back-end/src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
+import candidateRoutes from '@modules/candidate/infra/http/routes/candidate.routes';
 import employerRoutes from '@modules/employer/infra/http/routes/employer.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/employers', employerRoutes);
+routes.use('/candidates', candidateRoutes);
 
 export default routes;
