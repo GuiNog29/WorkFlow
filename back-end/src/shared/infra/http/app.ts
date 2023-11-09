3ffe6af02f9e1c72a15a6e7d27a6ea06b2f34f44
Guilherme Almeida <guimathiasgui@gmail.com>
2023-11-08 22:29:47 -0300

    Ajustando migrations e rotas

--- back-end/src/shared/infra/http/app.ts
+++ back-end/src/shared/infra/http/app.ts
@@ -1,15 +1,14 @@
-import 'express-async-errors';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import { AppError } from '@shared/errors/AppError';
-import employerRoutes from '@modules/employer/infra/http/routes/employer.routes';
-import candidateRoutes from '@modules/candidate/infra/http/routes/candidate.routes';
+import routes from './routes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/employers', employerRoutes);
-app.use('/candidates', candidateRoutes);
+app.use(routes);
+
 
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
   if(error instanceof AppError){
