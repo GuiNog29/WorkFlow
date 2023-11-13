825f02c4c78170168ffc96b6b6dc8a2ec8f8dd47
Guilherme Almeida <guimathiasgui@gmail.com>
2023-11-12 23:52:57 -0300

    Criando rota para salvar imagem do perfil

--- back-end/src/modules/employer/infra/http/routes/employer.routes.ts
+++ back-end/src/modules/employer/infra/http/routes/employer.routes.ts
@@ -1,14 +1,24 @@
+import multer from 'multer';
 import { Router } from 'express';
+import uploadConfig from '@config/upload';
 import EmployerController from '../controllers/EmployerController';
 import isAuthenticated from '@shared/infra/http/middlewares/isAuthenticated';
-import multer from 'multer';
+import UpdateProfilePictureEmployerController from '../controllers/UpdateProfilePictureEmployerController';
 
 const employerRoutes = Router();
+const upload = multer(uploadConfig);
 const employerController = new EmployerController();
+const updateProfilePictureController = new UpdateProfilePictureEmployerController();
 
 employerRoutes.post('/createEmployer', employerController.createEmployer);
 employerRoutes.put('/updateEmployer/:id', isAuthenticated, employerController.updateEmployer);
 employerRoutes.get('/getEmployer/:id', isAuthenticated, employerController.getEmployerById);
 employerRoutes.delete('/deleteEmployer/:id', isAuthenticated, employerController.deleteEmployer);
+employerRoutes.patch(
+  '/profilePicture',
+  isAuthenticated,
+  upload.single('profilePicture'),
+  updateProfilePictureController.update,
+);
 
 export default employerRoutes;
