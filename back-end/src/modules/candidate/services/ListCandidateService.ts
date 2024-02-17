8b33e02f23b610a964fa221b08036d1d9e2c4a9a
Guilherme Almeida <guimathiasgui@gmail.com>
2024-02-17 19:37:01 -0300

    Ajustando busca de candidatos para armazenar a busca em cache

--- back-end/src/modules/candidate/services/ListCandidateService.ts
+++ back-end/src/modules/candidate/services/ListCandidateService.ts
@@ -1,5 +1,7 @@
 import { ICandidatePaginate } from '../domain/models/ICandidatePaginate';
+import { Candidate } from '../entities/Candidate';
 import { CandidateRepository } from '../repositories/CandidateRepository';
+import RedisCache from '@shared/cache/RedisCache';
 
 interface SearchParams {
   page: number;
@@ -13,10 +15,23 @@ export class ListCandidateService {
     this.candidateRepository = new CandidateRepository();
   }
 
-  public async execute({ page, limit }: SearchParams): Promise<ICandidatePaginate> {
+  public async execute({ page, limit }: SearchParams): Promise<ICandidatePaginate | null> {
+    const redisCache = new RedisCache();
     const take = limit;
     const skip = (Number(page) - 1) * take;
-    const candidates = this.candidateRepository.findAll({ page, skip, take });
+
+    let candidates: ICandidatePaginate | null = await redisCache.recover<ICandidatePaginate>(
+      'workflow-CANDIDATES_LIST',
+    );
+
+    if (!candidates) {
+      const candidatesData = await this.candidateRepository.findAll({ page, skip, take });
+      if (candidatesData) {
+        candidates = candidatesData;
+        await redisCache.save('workflow-CANDIDATES_LIST', candidatesData);
+      }
+    }
+
     return candidates;
   }
 }
