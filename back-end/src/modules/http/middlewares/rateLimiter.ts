76dc3e3a934974cb8bca22612631401c66bac7c4
Guilherme Almeida <guimathiasgui@gmail.com>
2024-02-18 21:28:35 -0300

    Criando protecao para muitas requisicoes no sistema limitando

---
+++ back-end/src/modules/http/middlewares/rateLimiter.ts
@@ -0,0 +1,31 @@
+import Redis from 'ioredis';
+import { AppError } from '@shared/exceptions/AppError';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import { Request, Response, NextFunction } from 'express';
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    const redisClient = new Redis({
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+      password: process.env.REDIS_PASS || undefined,
+    });
+
+    const limiter = new RateLimiterRedis({
+      storeClient: redisClient,
+      keyPrefix: 'ratelimit',
+      points: 5,
+      duration: 1,
+    });
+
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (error) {
+    throw new AppError('Muitas requisições.', 429);
+  }
+}
