8d625706fa28e4e87b488f3bf0ac9e7cd3839b1f
Guilherme Almeida <guimathiasgui@gmail.com>
2024-05-20 22:10:19 -0300

    Ajustando front e container

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import imagemTelaInicial from './img/img_group_115.png';
+import ballonImg from './img/ballon.png';
+import clotesImg from './img/clotes.png';
 
 const App = () => {
   return (
     <div style={{ fontFamily: 'Arial, sans-serif', maxWidth: '430px', margin: 'auto', backgroundColor: '#fff', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
-      <img src={imagemTelaInicial} alt="Balloon Illustration" style={{ width: '100%' }} />
+      <div style={{ position: 'relative' }}>
+        <img src={ballonImg} alt="Balloon Illustration" style={{ width: '100%' }} />
+        <img src={clotesImg} alt="Orange Image" style={{ position: 'absolute', bottom: '20px', left: '50%', transform: 'translateX(-50%)', width: '40px', height: 'auto', border: '2px solid red' }} />
+      </div>
       <div style={{ padding: '20px', textAlign: 'center' }}>
         <h1 style={{ color: '#000' }}>O banco de dados inteligente e ágil!</h1>
         <p style={{ color: '#555', fontSize: '16px', marginTop: '10px' }}>Conectando empresas & profissionais qualificados.</p>
